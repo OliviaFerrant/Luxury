dbe909b6b0c697a3788d89919f843de4ef4f38c6
Thomas Ferrant <ferrantthomas@hotmail.fr>
2019-05-28 12:09:30 +0900

    added the authorize on the items controller and setup the authorization to the item policy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,18 +2,21 @@ class ItemsController < ApplicationController
   before_action :set_items, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all
+    @items = policy_scope(Item).order(brand: :asc)
   end
 
   def show
+    authorized @item
   end
 
   def new
     @item = Item.new
+    authorized @item
   end
 
   def create
     @item = Item.new(item_params)
+    authorized @item
     @item.user = current_user
     if @item.save
       redirect_to item_path(@item)
@@ -23,9 +26,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    authorized @item
   end
 
   def update
+    authorized @item
     if @item.update
       redirect_to item_path(@item)
     else
@@ -34,6 +39,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    authorized @item
     @item.destroy
     redirect_to items_path
   end
--- app/policies/item_policy.rb
+++ app/policies/item_policy.rb
@@ -4,4 +4,26 @@ class ItemPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    user_is_owner?
+  end
+
+  def destroy
+    user_is_owner?
+  end
+
+  private
+
+  def user_is_owner?
+    @record.user == @user
+  end
 end
