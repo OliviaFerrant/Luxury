2091b6a2e801d8a79697ecd8311ac7d622e19b04
Thomas Ferrant <ferrantthomas@hotmail.fr>
2019-05-28 12:19:56 +0900

    created admin user

--- app/policies/item_policy.rb
+++ app/policies/item_policy.rb
@@ -14,16 +14,16 @@ class ItemPolicy < ApplicationPolicy
   end
 
   def update?
-    user_is_owner?
+    user_is_owner_or_admin?
   end
 
   def destroy
-    user_is_owner?
+    user_is_owner_or_admin?
   end
 
   private
 
-  def user_is_owner?
-    @record.user == @user
+  def user_is_owner_or_admin?
+    @record.user == @user || @user.admin
   end
 end
