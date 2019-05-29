bac4c7688aa31d527c637c7e02ffcbcde530f97d
Louisa Wong <lou.h.wong@gmail.com>
2019-05-29 11:55:57 +0900

    added env file with thomas cloudinary key

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+items = Item.create([{ name: 'Yacht-Master 42' }, { name: 'Sea Dweller'}, { name: 'Cellini'}])
