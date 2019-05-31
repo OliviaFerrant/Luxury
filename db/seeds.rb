b36c20e8ee354215933ec664e2df2fea74e4d5e6
Thomas Ferrant <ferrantthomas@hotmail.fr>
2019-05-31 11:13:42 +0900

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'faker'
+
+brands = ["Patek Philippe", "Audemars Piguet", "Vacheron Constantin", "TagHeuer", "LangeSöhne", "Jaeger-LeCoultre", "Rolex", "Hublot", "Breguet & Fils", "Chopard", "Girard-Perregaux", "Blancpain", "Cartier", "IWC Schaffhausen", "Ulysse Nardin", "Panerai", "Piaget", "Breitling", "Omega", "Montblanc"]
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,15 +8,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "clearing database..."
+Item.delete_all
 
-items = Item.new(
-  name: 'Sea Dweller',
-  photo: 'https://res.cloudinary.com/louixa/image/upload/v1559099736/Screenshot_2019-05-29_12.14.40_cld4fq.png',
-  category: 'watch',
-  brand: 'Rolex',
-  price: 200,
-  address: 'Test'  )
-
-items.user = User.first
-items.save
-
+puts "Creating seeds"
+20.times do
+  name: Faker::
+  photo:
+  category: "Watches"
+  brand: brands.sample
+  address: Faker::Address.full_address
+  price: Faker::Commerce.price(range = 100..500)
+end
