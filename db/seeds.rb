9ab0307f36ff8fc4a6b71183ad3d77c841589e62
Thomas Ferrant <ferrantthomas@hotmail.fr>
2019-05-31 09:16:23 +0900

    added faker gem for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -17,3 +18,5 @@ items = Item.new(
 items.user = User.first
 items.save
 
+50.times do
+  address: Faker::Address.full_address
