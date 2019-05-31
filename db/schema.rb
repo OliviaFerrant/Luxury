16180682f93a4080a3da0e0d8f7a3904ea3a700e
Thomas Ferrant <ferrantthomas@hotmail.fr>
2019-05-31 11:48:06 +0900

    added description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_065154) do
+ActiveRecord::Schema.define(version: 2019_05_31_024309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_065154) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
+    t.text "description"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -37,6 +38,17 @@ ActiveRecord::Schema.define(version: 2019_05_28_065154) do
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_rentals_on_item_id"
     t.index ["user_id"], name: "index_rentals_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "description"
+    t.integer "rating"
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_reviews_on_item_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +69,6 @@ ActiveRecord::Schema.define(version: 2019_05_28_065154) do
   add_foreign_key "items", "users"
   add_foreign_key "rentals", "items"
   add_foreign_key "rentals", "users"
+  add_foreign_key "reviews", "items"
+  add_foreign_key "reviews", "users"
 end
