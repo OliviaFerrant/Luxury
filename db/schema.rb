a4499435a1f8b75b43c0031fbf969f77663d2b48
Thomas Ferrant <ferrantthomas@hotmail.fr>
2019-05-31 11:54:15 +0900

    added description as text, working.

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_31_024309) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photo"
-    t.string "description"
+    t.text "description"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
