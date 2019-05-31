368df4d7ff8e64bcf77284c07e32df78fd97837d
Jun Miyazaki <jun.miyazaki918@gmail.com>
2019-05-31 14:17:22 +0900

    made calendar size bigger

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_035744) do
+ActiveRecord::Schema.define(version: 2019_05_31_024309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
